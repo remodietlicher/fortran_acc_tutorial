7c8583901c3c92b97ea4a106f9691a81f2260d94
Remo Dietlicher <remo.dietlicher@meteoswiss.ch>
2020-01-24 17:45:28 +0100

    async in class test

--- src/the_program.f90
+++ src/the_program.f90
@@ -12,25 +12,29 @@ SUBROUTINE class_test
     USE the_class, ONLY: base
 
     TYPE(base) :: obj
-    INTEGER :: tmp(n)
+    INTEGER :: tmp(n), i
 
     CALL obj%alloc
 
     obj%member1(:) = 0.0
     obj%member2(:) = 1.0
 
-    !$acc update device(obj%member1, obj%member2)
+    !$acc update device(obj%member1, obj%member2) async
     !$acc data create(tmp)
 
     TIME_LOOP: DO i=1,10
-        !$acc kernels default(present)
-        tmp(:) = obj%member2(:)
-        obj%member2(:) = obj%member1(:) + obj%member2(:)
-        obj%member1(:) = tmp(:)
-        !$acc end kernels
+        !$acc parallel default(present) async
+        !$acc loop gang vector
+        DO i=1,n
+            tmp(i) = obj%member2(i)
+            obj%member2(i) = obj%member1(i) + obj%member2(i)
+            obj%member1(i) = tmp(i)
+        ENDDO
+        !$acc end parallel
     ENDDO TIME_LOOP
 
-    !$acc update host(obj%member1)
+    !$acc update host(obj%member1) async
+    !$acc wait
     !$acc end data
 
     IF(obj%member1(1) == 55) THEN
